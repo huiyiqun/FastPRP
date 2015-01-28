d44f69591f2bf9c950d06792cef436b090dc17cf
惠轶群 <huiyiqun@gmail.com>
2015-01-28 12:32:16 +0800

    delete functions which have moved to convert.pyx

--- RandomPermute.py
+++ RandomPermute.py
@@ -16,42 +16,6 @@ def cached(f):
     return g
 
 
-#def uint2bytes(uint, length):
-    #ret = [0] * length
-    #for i in range(length):
-        #ret[i] = uint & 255
-        #uint >>= 8
-        #if uint == 0:
-            #break
-    #else:
-        #print(uint, length)
-        #raise InputNotInRange()
-    #return bytes(reversed(ret))
-
-
-#def uint2bools(uint, length):
-    #ret = [False] * length
-    #for i in range(length):
-        #ret[i] = (uint & 1 == 1)
-        #uint >>= 1
-        #if uint == 0:
-            #return ret
-    #raise InputNotInRange()
-
-
-#def bytes2bools(bytes):
-    #ret = []
-    #for b in reversed(bytes):
-        #single_byte = [False] * 8
-        #for i in range(8):
-            #single_byte[i] = (b & 1 == 1)
-            #b >>= 1
-            #if b == 0:
-                #break
-        #ret += single_byte
-    #return ret
-
-
 class BitArray(object):
     '''
     For BitArray from bitstring is not efficient,
