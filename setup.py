79d9acc2ae02b655889a33ebd7838cfc6999b3d6
惠轶群 <huiyiqun@gmail.com>
2015-01-28 15:31:00 +0800

    PEP8 fix

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-  name = 'Fast tools to covert among uint, bytes and list of bools',
-  ext_modules = cythonize("convert.pyx"),
+    name='Fast tools to covert among uint, bytes and list of bools',
+    ext_modules=cythonize("convert.pyx"),
 )
