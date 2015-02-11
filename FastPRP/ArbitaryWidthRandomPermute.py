4f2a99605b3c2be0e215a07e7c4d711b0c024944
惠轶群 <huiyiqun@gmail.com>
2015-02-11 18:27:39 +0800

    add test code for multiprocessing programming

--- FastPRP/ArbitaryWidthRandomPermute.py
+++ FastPRP/ArbitaryWidthRandomPermute.py
@@ -14,10 +14,13 @@ class ArbitaryWidthRandomPermute(object):
     _grouped_by = 4
 
     def __init__(self, key, width, keeped=None):
-        self.keeped = keeped if keeped is not None else (lambda x: False)
+        self.keeped = keeped if keeped is not None else self._keeped
         self.width = width
         self.key = key
         self.random_permuters = dict()
+
+    def _keeped(self, x):
+        return False
 
     def _in_range(self, output):
         return 10 ** (self.width - 1) <= output < 10 ** self.width
@@ -51,7 +54,7 @@ if __name__ == '__main__':
     from datetime import datetime
 
     # test speed
-    for N in range(2, 30):
+    for N in range(3, 30):
         start_t = datetime.now()
         awrp = ArbitaryWidthRandomPermute(1008611, N)
         for i in range(100):
@@ -61,16 +64,88 @@ if __name__ == '__main__':
 
     # test validity
     test_bits = 5
+    def test_validity(awrp, i, found):
+        res = awrp.permute(i)
+        #print('%d -->' % i, res)
+        if res is None:
+            return
+        if found:
+            if hasattr(found, 'lock'):
+                with found.lock:
+                    if res in found:
+                        raise Exception('Output is not unique')
+                    found.add(res)
+                    #print('Length of found is %d.' % len(found))
+            else:
+                if res in found:
+                    raise Exception('Output is not unique')
+                found.add(res)
+                #print('Length of found is %d.' % len(found))
+        if not 10 ** (test_bits-1) <= res < 10 ** test_bits:
+            raise Exception('Output is not in given range')
+
+    # single thread
+    start_t = datetime.now()
     awrp = ArbitaryWidthRandomPermute(1008611, test_bits)
     found = set()
     for i in range(10 ** test_bits):
+        test_validity(awrp, i, found)
+    end_t = datetime.now()
+    print('%d unique number generated.' % len(found))
+    print('it takes %s' % (end_t - start_t))
+
+    # multithreading
+    # Because of GIL, multithread gain no efficency.
+    # whatever, this module is thread-safe.
+    from threading import Thread, Lock
+    start_t = datetime.now()
+    awrp = ArbitaryWidthRandomPermute(1008611, test_bits)
+    class set(set, object):
+        pass
+    found = set()
+    found.lock = Lock()
+    threads = list()
+    for i in range(10 ** test_bits):
+        t = Thread(target=test_validity, args=(awrp, i, found))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+    end_t = datetime.now()
+    print('%d unique number generated.' % len(found))
+    print('it takes %s' % (end_t - start_t))
+
+    # multiprocess
+    def test_validity_with_shared_memory(args):
+        shared, lock, r2s, i = args
+        # get the object from proxy, then notify the change
+        awrp = shared.awrp
         res = awrp.permute(i)
-        print('%d -->' % i, res)
+        shared.awrp = awrp
+        #print('%d -->' % i, res)
         if res is None:
-            continue
-        if res in found:
-            raise Exception('Output is not unique')
+            return
+        with lock:
+            if res in found:
+                raise Exception('Output is not unique')
+            found[res] = i
+            #print('Length of found is %d.' % len(found))
+
         if not 10 ** (test_bits-1) <= res < 10 ** test_bits:
             raise Exception('Output is not in given range')
-        found.add(res)
-    print('%d unique number generated.' % len(found))
+
+
+    from multiprocessing import Pool, Manager, Lock
+    start_t = datetime.now()
+    mgr = Manager()
+    shared = mgr.Namespace()
+    shared.awrp = ArbitaryWidthRandomPermute(1008611, test_bits)
+    result_to_source = mgr.dict()
+    lock = mgr.Lock()
+    pool = Pool(processes=8)
+    pool.map(test_validity_with_shared_memory, ((shared, lock, result_to_source, i) for i in range(10 ** test_bits)))
+    pool.close()
+    pool.join()
+    end_t = datetime.now()
+    print('%d unique number generated.' % len(shared.found))
+    print('it takes %s' % (end_t - start_t))
